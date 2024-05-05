11e1a3dd58797293432320a07e8e5391247c1015
marcemarch <march.aruquipa@gmail.com>
2024-05-05 09:59:23 -0400

    fear: "operaciones"

--- Hola.java
+++ Hola.java
@@ -1,5 +1,8 @@
 class Hola{
     public static void main(String[] args) {
         System.out.println("Hola mundo");
-    }
+        int a = 55;
+        int c = 88;
+        System.out.println("resultado de: " + (a+c));
+    } 
 }
